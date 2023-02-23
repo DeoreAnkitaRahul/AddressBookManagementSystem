badcb4193925fc336ec8339388ba1e39787c0b75
“DeoreAnkitaRahul” <ankitapatil375@gmail.com>
2023-02-23 13:19:26 +0530

    Welcome to The Address Book System

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿namespace AddressBookManagementSystem
+{
+    public class Program
+    {
+        public static void Main(String[] args)
+        {
+            Console.WriteLine("Welcome to The Address Book System");
+     PersonDetails  Details= new PersonDetails("ankita", "patil", "hjkl", "kolhapur", 7588, "Maharashtra",1234567891 , "ankitardeore.com");
+
+
+        }
+    }
+}
