3d2448192d4e925877ee56f01dd7ba8331fda100
“DeoreAnkitaRahul” <ankitapatil375@gmail.com>
2023-02-23 13:54:09 +0530

    Add New Person Contact

--- Program.cs
+++ Program.cs
@@ -1,12 +1,17 @@
-﻿namespace AddressBookManagementSystem
+﻿using static AddressBookManagementSystem.PersonDetails;
+
+namespace AddressBookManagementSystem
 {
     public class Program
     {
         public static void Main(String[] args)
         {
             Console.WriteLine("Welcome to The Address Book System");
-     PersonDetails  Details= new PersonDetails("ankita", "patil", "hjkl", "kolhapur", 7588, "Maharashtra",1234567891 , "ankitardeore.com");
+            //PersonDetails  Details= new PersonDetails("ankita", "patil", "hjkl", "kolhapur", 7588, "Maharashtra",1234567891 , "ankitardeore.com");
+            PersonDetails details = new PersonDetails();
 
+           details.AddDetails();
+            details.ListingPeople();
 
         }
     }
